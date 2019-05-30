c5e8ace0df6c9fed483ccb9426e8306ced2008e1
Derek Jones <derek@derekjones.io>
2019-05-29 21:34:57 -0400

    Complete Express Middleware guided demo

--- hubs/hubs-router.js
+++ hubs/hubs-router.js
@@ -21,25 +21,11 @@ router.get('/', async (req, res) => {
 
 // /api/hubs/:id
 
-router.get('/:id', async (req, res) => {
-  try {
-    const hub = await Hubs.findById(req.params.id);
-
-    if (hub) {
-      res.status(200).json(hub);
-    } else {
-      res.status(404).json({ message: 'Hub not found' });
-    }
-  } catch (error) {
-    // log error to server
-    console.log(error);
-    res.status(500).json({
-      message: 'Error retrieving the hub',
-    });
-  }
+router.get('/:id', validateId, async (req, res) => {
+  res.status(200).json(req.hub);
 });
 
-router.post('/', async (req, res) => {
+router.post('/', requiredBody, async (req, res) => {
   try {
     const hub = await Hubs.add(req.body);
     res.status(201).json(hub);
@@ -52,7 +38,7 @@ router.post('/', async (req, res) => {
   }
 });
 
-router.delete('/:id', async (req, res) => {
+router.delete('/:id', validateId, async (req, res) => {
   try {
     const count = await Hubs.remove(req.params.id);
     if (count > 0) {
@@ -69,7 +55,7 @@ router.delete('/:id', async (req, res) => {
   }
 });
 
-router.put('/:id', async (req, res) => {
+router.put('/:id', validateId, requiredBody, async (req, res) => {
   try {
     const hub = await Hubs.update(req.params.id, req.body);
     if (hub) {
@@ -88,7 +74,7 @@ router.put('/:id', async (req, res) => {
 
 // add an endpoint that returns all the messages for a hub
 // this is a sub-route or sub-resource
-router.get('/:id/messages', async (req, res) => {
+router.get('/:id/messages', validateId, async (req, res) => {
   try {
     const messages = await Hubs.findHubMessages(req.params.id);
 
@@ -103,7 +89,7 @@ router.get('/:id/messages', async (req, res) => {
 });
 
 // add an endpoint for adding new message to a hub
-router.post('/:id/messages', async (req, res) => {
+router.post('/:id/messages', validateId, requiredBody, async (req, res) => {
   const messageInfo = { ...req.body, hub_id: req.params.id };
 
   try {
@@ -117,5 +103,34 @@ router.post('/:id/messages', async (req, res) => {
     });
   }
 });
+
+async function validateId(req, res, next) {
+  try {
+    const { id } = req.params;
+    const hub = await Hubs.findById(id);
+
+    if (hub) {
+      req.hub = hub;
+      next();
+    } else {
+      res.status(404).json({ message: 'Hub not found; invalid id' });
+    }
+  } catch (err) {
+    res.status(500).json({ message: 'Failed to process request' });
+  }
+}
+
+function requiredBody(req, res, next) {
+  if (req.body && Object.keys(req.body).length) {
+    // when using next() WITHOUT an argument, it
+    // means go on to the next bit of middleware
+    next();
+  } else {
+    // when using next() WITH an argument, it
+    // means jump to error bit of middleware
+    next({ message: 'Please include request body' });
+    //res.status(400).json({ message: 'Please include request body' });
+  }
+}
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,20 +1,71 @@
 const express = require('express'); // importing a CommonJS module
+const helmet = require('helmet');
+const morgan = require('morgan');
 
 const hubsRouter = require('./hubs/hubs-router.js');
 
 const server = express();
 
+// GLOBAL MIDDLEWARE
+
+// built in middleware
 server.use(express.json());
 
+// third party middleware
+server.use(helmet());
+server.use(morgan('dev'));
+
+// custom middleware
+server.use(typeLogger);
+server.use(addName);
+//server.use(lockout);
+//server.use(moodyGatekeeper);
+
+// router
 server.use('/api/hubs', hubsRouter);
 
 server.get('/', (req, res) => {
-  const nameInsert = (req.name) ? ` ${req.name}` : '';
+  const nameInsert = req.name ? ` ${req.name}` : '';
 
   res.send(`
     <h2>Lambda Hubs API</h2>
-    <p>Welcome${nameInsert} to the Lambda Hubs API</p>
+    <p>Welcome ${nameInsert} to the Lambda Hubs API</p>
     `);
+});
+
+// custom middleware
+function typeLogger(req, res, next) {
+  console.log(`${req.method} Request`);
+  next();
+}
+
+function addName(req, res, next) {
+  req.name = req.name || 'Derek';
+  next();
+}
+
+function lockout(req, res, next) {
+  res.status(403).json({ message: 'API lockout!' });
+}
+
+function moodyGatekeeper(req, res, next) {
+  const seconds = new Date().getSeconds();
+
+  if (seconds % 3 === 0) {
+    res.status(403).json({ err: 'You shall not pass!' });
+  } else {
+    next();
+  }
+}
+
+// Global error catch
+// we want this immediately before our export
+server.use((err, req, res, next) => {
+  // only executes if next(err) is called with an argument
+  res.status(500).json({
+    message: 'Bad Panda',
+    err,
+  });
 });
 
 module.exports = server;
